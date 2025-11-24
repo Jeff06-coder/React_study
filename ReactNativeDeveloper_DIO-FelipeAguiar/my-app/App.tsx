0ff6d8ba39c84c2f5790a90d9566fa3d85c71826
Jeff06-coder <loplopado67@gmail.com>
2025-11-24 11:01:59 -0300

    Adicionando imagens ao app, criando styles condicionais e fazendo o typescript entender outros tipos(não funcionando por alguma razão)

--- ReactNativeDeveloper_DIO-FelipeAguiar/my-app/App.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/my-app/App.tsx
@@ -1,22 +1,36 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import symbolOff from './assets/pictures/symbol-off.png'; //Importando as imagens
+import symbolOn from './assets/pictures/symbol-on.png';
 
-import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+
+  const  isActive = false
+
   return (
-    <View style={styles.container}>
-    
-     
-      
-      
+    /*Colocando as imagens no app com um sistema de liga e desliga*/
+    <View style={isActive? styles.containerOn : styles.containerOff}>
+      <Image
+        source={isActive? symbolOn : symbolOff}>
+      </Image>
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  containerOn: {
+    flex: 1, //Faz "pegar" a tela inteira
+    backgroundColor: '#000000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerOff: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
---
+++ ReactNativeDeveloper_DIO-FelipeAguiar/my-app/index.d.ts
@@ -0,0 +1,2 @@
+//Fazendo o typeScript enterder outros tipos (mas por uma razão que não sei qual, não está funcionando)
+declare module '*.png'
