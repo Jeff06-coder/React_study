b7b14f4af91887a79f9e5294ff77d28b8f10d3f3
Jeff06-coder <loplopado67@gmail.com>
2025-11-24 13:32:52 -0300

    Fazendo a separação de pastas e arquivos

---
+++ ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/screens/Home/Index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import styles from './Style'
+
+export default function Home() {
+
+return(
+    <View style = {styles.container}>
+        <Text>AAAAAAAAAAAAAAAAA</Text>
+        <StatusBar style='auto'></StatusBar>
+    </View>
+    
+
+)
+
+}
+
