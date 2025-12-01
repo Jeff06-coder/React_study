f1bc06583b7836d1a781e968369d427c711ebf53
Jeff06-coder <loplopado67@gmail.com>
2025-12-01 14:42:47 -0300

    Terminando os botões

--- ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/components/BatButton/BatButton.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/components/BatButton/BatButton.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Button, Pressable, View, Text } from 'react-native';
+import { Pressable, View, Text } from 'react-native';
 
 import { styles } from './BatButtonStyle';
 
 export function BatButton() {
   return (
-    <View style={styles.container}>
-        <Button 
+    <>
 
-            onPress={() => {console.log("Ola")}}
-            title='Click aqui'
-            accessibilityLabel='click em mim'
-            color='rgba(0, 0, 0, 0.53)'
-
-        />
+        <Pressable>
+            <Text style={styles.text}>👌 Generate 👌</Text>
+        </Pressable>
         
         <Pressable>
-            <Text style={{ backgroundColor:'grey', color: 'white'}}>🤫 Copy 🤫</Text>
+            <Text style={styles.text}>🤫 Copy 🤫</Text>
         </Pressable>
-    </View>
+    </>
   );
 }
--- ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/components/BatButton/BatButtonStyle.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/components/BatButton/BatButtonStyle.tsx
@@ -1,7 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    
+  text:{
+
+    backgroundColor:'black',
+    color: 'white',
+    textAlign:'center',
+    fontSize: 15,
+    paddingHorizontal: 32,
+    elevation: 5,
+    marginTop: 10,
+    padding: 10
+
   }
 });
