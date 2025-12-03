859da22289e0deae6e9df27924c1da9a6499f7e5
Jeff06-coder <loplopado67@gmail.com>
2025-12-03 10:01:17 -0300

    Adicionando informações no README e terminando de fazer a lógica dos Button e os tornando funcional, prontos pra usar.

--- ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/components/BatButton/BatButton.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/Projects/bat-app/src/components/BatButton/BatButton.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Pressable, Text } from 'react-native';
-import { BATTextInput } from '../../components/TextInput/TextInput';
+import { BATTextInput } from '../../components/TextInput/TextInput'; //Para utilizar a interface tbm
 import { styles } from './BatButtonStyle';
-import generatePass from '../../services/passwordSarvice';
+import generatePass from '../../services/passwordSarvice'; //Trazendo o back(ação) para o Copy
 
+import * as Clipboard from 'expo-clipboard'; //Memória do CTRL + C e CTRL + V
 
 export function BatButton() {
     const [pass, setPass] = useState('')
@@ -13,18 +14,27 @@ export function BatButton() {
         setPass(generateToken)
     }
 
+    //Function que ativa o button copy
+    function handleCopyButton() {
+
+        Clipboard.setStringAsync(pass)
+
+    }
+
   return (
     <> 
 
         <BATTextInput pass={pass}/>
 
+        {/*Button de Gerar a senha, é um texto interativo, igual um <Button> praticamente*/}
         <Pressable
             onPress={handleGenerateButton}>
             <Text style={styles.text}>👌 Generate 👌</Text>
         </Pressable>
         
+        {/*Button de Copy, é um texto interativo, igual um <Button> praticamente*/}
         <Pressable>
-            <Text style={styles.text}>🤫 Copy 🤫</Text>
+            <Text style={styles.text} onPress={handleCopyButton}>🤫 Copy 🤫</Text>
         </Pressable>
     </>
   );
