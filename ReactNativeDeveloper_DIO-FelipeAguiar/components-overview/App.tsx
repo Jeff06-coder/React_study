9dc54988b55e5e240cd2997c2211f26f28770edd
Jeff06-coder <loplopado67@gmail.com>
2026-01-21 14:15:15 -0300

    Feito teste com onTouch e Alert na View

--- ReactNativeDeveloper_DIO-FelipeAguiar/components-overview/App.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/components-overview/App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+     <View onTouchStart={(event) => Alert.alert("event")} onTouchEnd={(evento)=> {Alert.alert('Éeee')}}>
+
+      <Text>Clique aqui!</Text>
+
+     </View>
+
     </View>
+
   );
 }
 
