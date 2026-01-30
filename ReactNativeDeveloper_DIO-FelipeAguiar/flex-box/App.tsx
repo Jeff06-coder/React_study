bfe6aaa28e8927fd3cd815f8de59fc7ffcdedc43
Jeff06-coder <loplopado67@gmail.com>
2026-01-30 08:25:26 -0300

    Aprendendo sobre o flex, ele preenche a tela, o 'item' que estiver com ele, vai preencher a tela ao todo, tem como colocar prioridade

--- ReactNativeDeveloper_DIO-FelipeAguiar/flex-box/App.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/flex-box/App.tsx
@@ -1,18 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function CaixaRed(){
+  return(
+    <View style={{width:150, height:150, backgroundColor:'red'}}></View>
+  )
+}
+
+function CaixaBlue(){
+  return(
+    <View style={{width:'100%', height:150, backgroundColor:'blue', flex:1}}></View>
+  )
+}
+ 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <CaixaRed />
+      <CaixaBlue />
+      <CaixaRed />
+      
+
+      
       <StatusBar style="auto" />
+
+
+
     </View>
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    borderWidth: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
