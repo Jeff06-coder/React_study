63dfca70f8f6b2f77b5f824bf5e24e6cbda32217
Jeff06-coder <loplopado67@gmail.com>
2026-01-30 14:43:40 -0300

    Usando os eventos onPress e onPressIn

--- ReactNativeDeveloper_DIO-FelipeAguiar/events-react_native/App.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/events-react_native/App.tsx
@@ -1,18 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+import { helloV } from './src/actions';
 import { StyleSheet, Text, View } from 'react-native';
+
+async function handlerPress(id: number){
+  console.log('Texto pressionado', id);
+  
+}
+
+async function handlerFunction(){
+  console.log('Texto pressionado - Handler Function');
+}
 
 export default function App() {
 
-  async function handlerPress(id: number){
-    console.log('Texto pressionado', id);
-    
-  }
-
-
-
   return (
     <View style={styles.container}>
-      <Text onPress={() => handlerPress(1)}>Open up App.tsx to start working on your app!</Text>
+
+      <Text
+       style={{fontFamily: 'Arial', fontSize: 20}} 
+       onPressIn={() => console.log("PressIn acionado (Aciona quando começa a pressionar)")}
+       onPress={() => console.log("Componente Principal Pressionado (Aciona quando solta)")}
+
+       
+        
+        
+        >Componente Principal</Text>
+
+
+      {/* <Text onPress={() => handlerPress(1)} style={{fontSize: 18,}}>Open up App.tsx to start working on your app!</Text>
+      <Text onPress={() => console.log("onPress Inline")} style={{fontSize: 20,}}>Inline</Text>
+      <Text onPress={handlerFunction} style={{fontSize: 20,}}>Handler Function</Text>
+      <Text onPress={() => handlerPress(2)} style={{fontSize: 20,}}>Handler Function with parameters</Text>
+
+      <Text onPress={helloV} style={{fontSize: 20,}}>Hello Viajante Another file</Text> */}
+
+
+
+        
     </View>
   );
 }
