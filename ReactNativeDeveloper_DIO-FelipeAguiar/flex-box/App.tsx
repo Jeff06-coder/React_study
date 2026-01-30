01623b929b6fc558aa7473f74b6797816f21dfcd
Jeff06-coder <loplopado67@gmail.com>
2026-01-30 08:54:28 -0300

    alingSelf muda apenas o eixo do elemneto colocado

--- ReactNativeDeveloper_DIO-FelipeAguiar/flex-box/App.tsx
+++ ReactNativeDeveloper_DIO-FelipeAguiar/flex-box/App.tsx
@@ -9,13 +9,13 @@ function CaixaRed(){
 
 function CaixaBlue(){
   return(
-    <View style={{width:100, height:100, backgroundColor:'blue'}}></View>
+    <View style={{width:100, height:100, backgroundColor:'blue', alignSelf:'flex-end'}}></View>
   )
 }
 
 function CaixaYellow(){
   return(
-    <View style={{width:100, height:100, backgroundColor:'yellow'}}></View>
+    <View style={{width:100, height:100, backgroundColor:'yellow', alignSelf:'flex-start'}}></View>
   )
 }
  
